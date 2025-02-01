f72022dee9801955a1e98f853382f858bb4eab2e
Yakumaa <maharjanshrish8@gmail.com>
2025-02-01 22:25:15 +0545

    first commit

---
+++ custom_elements.js
@@ -0,0 +1,81 @@
+class PopupInfo extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    //create a shadow root
+    const shadow = this.attachShadow({ mode: "open"});
+
+    //create spans
+    const wrapper = document.createElement("span");
+    wrapper.setAttribute("class", "wrapper");
+
+    const icon = document.createElement("span");
+    icon.setAttribute("class", "icon");
+    icon.setAttribute("tabindex", 0);
+
+    const info = document.createElement("span");
+    info.setAttribute("class", "info");
+
+    //take attribute content and put it inside the info span
+    const text = this.getAttribute("data-text");
+    info.textContent = text;
+
+    //insert icon
+    let imgUrl;
+    if (this.hasAttribute("img")) {
+      imgUrl = this.getAttribute("img");
+    } else {
+      imgUrl = "img/default.png";
+    }
+
+    const img = document.createElement("img")
+    img.src = imgUrl;
+    icon.appendChild(img);
+
+    //create some css to apply to the shadow dom
+    const style = document.createElement("style");
+    console.log(style.isConnected);
+
+    style.textContent = `
+      .wrapper {
+        position: relative;
+      }
+
+      .info {
+        font-size: 0.8rem;
+        width: 200px;
+        display: inline-block;
+        border: 1px solid black;
+        padding: 10px;
+        background: white;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0,0,0,0.2);
+        opacity: 0;
+        transition: 0.6s all;
+        position: absolute;
+        bottom: 20px;
+        left: 10px;
+        z-index: 3;
+      }
+
+      img {
+        width: 1.2rem;
+      }
+
+      .icon:hover + .info, .icon:focus + .info {
+        opacity: 1;
+      }
+    `;
+
+    //attach the created elements to the shadow dom
+    shadow.appendChild(style);
+    console.log(style.isConnected);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(icon);
+    wrapper.appendChild(info);
+  }
+}
+
+customElements.define("popup-info", PopupInfo);
